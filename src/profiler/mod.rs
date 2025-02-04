dd1b6357f4bd9a82c5050e4e0a2819ec8ecefe00
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-02-04 16:17:59 +0100

    feat: enhance run_profiler function with improved thread information logging and error handling

--- src/profiler/mod.rs
+++ src/profiler/mod.rs
@@ -1,52 +1,58 @@
 #![allow(unused_variables)]
 use crate::logs;
-use mach2::mach_init::mach_thread_self;
+use libc::exit;
 use mach2::traps::mach_task_self;
 use mach2::traps::task_for_pid;
 embed_plist::embed_info_plist!("../../Info.plist");
 
 pub fn run_profiler(pid: &i32) {
     logs::rp_log("Start running the profiler...");
+    let num_logical_cores: usize = num_cpus::get();
     let mut task: u32 = 0;
+
+    // kernel error code
     let kernel_success: i32 = 0;
-    //TODO
-    // fix segfault by expanding the unsafe block into smaller unsafe block
-    //Segfault caused by the unsafe block to huge and to much pointer or reference access inside ?
+
+    // threads mutable var
+    let mut thread_list: *mut u32 = std::ptr::null_mut();
+    let mut thread_count: u32 = 0;
+    // buffer for the thead_info output
+    let mut thread_info_out: [i32; 1024] = [0; 1024];
+    let mut thread_info_out_cnt: u32 = 1024;
+
+    // flavor
+    let thread_basic_info: u32 = 3;
+    let thread_id_info: u32 = 4;
+    let thread_extended_info: u32 = 5;
     unsafe {
         // mach_task_self(): This function returns the task port
         // for the current process.
         // Essentially, it provides a reference
         // to the current process's task.
         let task_pid = task_for_pid(mach_task_self(), *pid, &mut task);
+
         if task_pid != kernel_success {
             logs::error_log_with_code(
                 "Error attaching the process to task_for_pid. Code:".to_string(),
                 task_pid.to_string(),
             );
+            exit(1);
         }
-        let mut thread_list: *mut u32 = std::ptr::null_mut();
-        let mut thread_count: u32 = 0;
         let task_thread = libc::task_threads(task, &mut thread_list, &mut thread_count);
-        println!(
-            "memory where the threads are written: {:?} number of threads of the given process: {}",
-            thread_list, thread_count
-        );
 
         if task_thread != kernel_success {
             logs::error_log_with_code(
                 "Error getting the thread for the given pid. Code:".to_string(),
                 task_pid.to_string(),
             );
+            exit(1);
         }
 
-        // Thread flavor
-        let thread_basic_info: u32 = 3;
-        let thread_id_info: u32 = 4;
-        let thread_sched_info: u32 = 5;
-        // let thread_debug_info: u32 = 7;
+        println!(
+            "memory where the threads are written: {:?} number of threads of the given process: {}",
+            thread_list, thread_count
+        );
 
-        let mut thread_info_out: [i32; 1024] = [0; 1024]; // Define the thread_info_out array
-        let mut thread_info_out_cnt: u32 = 1024; // Define the thread_info_out_cnt
         let thread_info = libc::thread_info(
             *thread_list,
             thread_basic_info,
@@ -56,16 +62,23 @@ pub fn run_profiler(pid: &i32) {
 
         if thread_info != kernel_success {
             logs::error_log_with_code(
-                "Error getting the thread for the given pid. Code:".to_string(),
+                "Error getting info from mach for the given thread. Code:".to_string(),
                 task_pid.to_string(),
             );
+            exit(1);
         }
 
-        for line in thread_info_out {
-            if line != 0 {
-                println!("{}", line);
-            }
-        }
+        // println!("{:?}", thread_info_out);
+        println!(
+            "user run time: {}.{:06}ms",
+            thread_info_out[0], thread_info_out[1]
+        );
+        println!(
+            "system time: {}.{:06}ms",
+            thread_info_out[2], thread_info_out[3]
+        );
+
+        println!("{}% scaled cpu usage", thread_info_out[4]);
 
         // let address: u64 = 0; // Define the address
         // let size: u64 = 0; // Define the size
