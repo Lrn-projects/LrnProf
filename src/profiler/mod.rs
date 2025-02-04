471b9e5ad5cafb7bc25c48f3a934e9274938d332
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-02-04 12:33:33 +0100

    feat: enhance run_profiler function with improved thread information retrieval and error logging

--- src/profiler/mod.rs
+++ src/profiler/mod.rs
@@ -1,4 +1,6 @@
+#![allow(unused_variables)]
 use crate::logs;
+use mach2::mach_init::mach_thread_self;
 use mach2::traps::mach_task_self;
 use mach2::traps::task_for_pid;
 embed_plist::embed_info_plist!("../../Info.plist");
@@ -24,26 +26,48 @@ pub fn run_profiler(pid: &i32) {
         }
         let mut thread_list: *mut u32 = std::ptr::null_mut();
         let mut thread_count: u32 = 0;
-        libc::task_threads(task, &mut thread_list, &mut thread_count);
+        let task_thread = libc::task_threads(task, &mut thread_list, &mut thread_count);
         println!(
             "memory where the threads are written: {:?} number of threads of the given process: {}",
             thread_list, thread_count
         );
 
-        let flavor: u32 = 0; // Define the flavor
-        // let mut thread_info_out: [i32; 1024] = [0; 1024]; // Define the thread_info_out array
-        // let mut thread_info_out_cnt: u32 = 1024; // Define the thread_info_out_cnt
-        // let thread_info = libc::thread_info(
-        //     task,
-        //     flavor,
-        //     thread_info_out.as_mut_ptr(),
-        //     &mut thread_info_out_cnt,
-        // );
-        let mut policy_info: *mut i32 = std::ptr::null_mut();
-        let mut count: *mut u32 = std::ptr::null_mut();
-        let mut get_default: *mut i32 = std::ptr::null_mut();
-        let thread_state = libc::thread_policy_get(task, flavor, policy_info, count, get_default);
-        println!("{:?}", thread_state);
+        if task_thread != kernel_success {
+            logs::error_log_with_code(
+                "Error getting the thread for the given pid. Code:".to_string(),
+                task_pid.to_string(),
+            );
+        }
+
+        // Thread flavor
+        let thread_basic_info: u32 = 3;
+        let thread_id_info: u32 = 4;
+        let thread_sched_info: u32 = 5;
+        // let thread_debug_info: u32 = 7;
+
+        let mut thread_info_out: [i32; 1024] = [0; 1024]; // Define the thread_info_out array
+        let mut thread_info_out_cnt: u32 = 1024; // Define the thread_info_out_cnt
+        let thread_info = libc::thread_info(
+            *thread_list,
+            thread_id_info,
+            thread_info_out.as_mut_ptr(),
+            &mut thread_info_out_cnt,
+        );
+
+        if thread_info != kernel_success {
+            logs::error_log_with_code(
+                "Error getting the thread for the given pid. Code:".to_string(),
+                task_pid.to_string(),
+            );
+        }
+
+        // println!("{:?}", thread_info_out);
+        for line in thread_info_out {
+            if line != 0 {
+                println!("{}", line);
+            }
+        }
+
         // let address: u64 = 0; // Define the address
         // let size: u64 = 0; // Define the size
         // let data: *mut usize = std::ptr::null_mut(); // Define the data pointer
