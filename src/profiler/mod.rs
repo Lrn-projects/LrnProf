0d8717ff9019c70fd8dc811deaefef2a58816a8d
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-02-11 17:41:18 +0100

    chore: rename var and function with better name and improve readability

--- src/profiler/mod.rs
+++ src/profiler/mod.rs
@@ -24,12 +24,27 @@ pub fn run_profiler(pid: &i32) {
     // buffer for the thead_info output
     let mut thread_info_out: [i32; 1024] = [0; 1024];
     let mut thread_info_out_cnt: u32 = 1024;
+    // used for the thread_get_state
+    let mut new_state: [u64; 129] = [0; 129];
+    let mut new_state_count: u32 = 129;
     // backtrace
+
+    // buffers
+    let mut process_address_bytes_buffer = [0; 128];
+    let mut next_fp_bytes_vec = [0u8; 8];
+    let mut next_lr_bytes_vec = [0u8; 8];
+
+    // used to read the process memory address
+    let array: [u64; 128] = [0; 128];
+    let size: usize = array.len();
 
     // flavor
     let thread_basic_info: u32 = 3;
     let thread_id_info: u32 = 4;
     let thread_extended_info: u32 = 5;
+
+    //unwind loop
+    let mut addresses: Vec<u64> = Vec::new();
 
     //callback
     let cb = |symbol: &backtrace::Symbol| {
@@ -81,8 +96,6 @@ pub fn run_profiler(pid: &i32) {
             exit(1);
         }
 
-        let mut new_state: [u64; 129] = [0; 129];
-        let mut new_state_count: u32 = 129;
         let thread_state = mach2::thread_act::thread_get_state(
             *thread_list,
             6,
@@ -102,28 +115,28 @@ pub fn run_profiler(pid: &i32) {
         // program pointer
         let PC = new_state[32];
 
-        //unwind loop
-        let mut addresses: Vec<u64> = Vec::new();
-
         if FP == 0 {
             panic!("FP is 0 cannot unreferenced");
         }
 
         let fp_ptr = FP as *const u64;
 
-        let array: [u64; 128] = [0; 128];
-        let size: usize = array.len();
-
         let pid_i32 = *pid as i32;
         parser::parse_bin(*pid);
         if !fp_ptr.is_null() {
             loop {
-                let mut bytes_buffer = [0; 128];
-                let test = test(pid_i32, FP as usize, size, &mut bytes_buffer).unwrap();
-                let mut next_fp_bytes_vec = [0u8; 8];
-                let mut next_lr_bytes_vec = [0u8; 8];
-                next_fp_bytes_vec.copy_from_slice(&bytes_buffer[..8]);
-                next_lr_bytes_vec.copy_from_slice(&bytes_buffer[8..16]);
+                let read_process_address = read_process_address(
+                    pid_i32,
+                    FP as usize,
+                    size,
+                    &mut process_address_bytes_buffer,
+                )
+                .unwrap();
+
+                // load the specific chunk of the buffer into vector
+                // used to get the next_fp and next_lr addresses
+                next_fp_bytes_vec.copy_from_slice(&process_address_bytes_buffer[..8]);
+                next_lr_bytes_vec.copy_from_slice(&process_address_bytes_buffer[8..16]);
                 let next_fp = u64::from_le_bytes(next_fp_bytes_vec);
                 let next_lr = u64::from_le_bytes(next_lr_bytes_vec);
                 addresses.push(next_lr);
@@ -153,7 +166,32 @@ pub fn run_profiler(pid: &i32) {
     // println!("backtrace info {:?}", buf);
 }
 
-fn test(pid: Pid, address: usize, size: usize, buff: *mut [u8; 128]) -> std::io::Result<()> {
+/// The function `read_process_address` in Rust takes a process ID, address, size, and buffer pointer, reads memory from
+/// the specified address in the process, and copies it into the buffer.
+///
+/// Arguments:
+///
+/// * `pid`: The `pid` parameter in the function `read_process_address` is of type `Pid`, which likely represents a
+/// process identifier.
+/// * `address`: The `address` parameter in the `read_process_address` function represents the memory address from which
+/// data needs to be read. It is of type `usize`, which is an unsigned integer type representing memory
+/// addresses in Rust.
+/// * `size`: The `size` parameter in the `read_process_address` function represents the number of bytes to copy from
+/// the memory address specified by `address`. It indicates the size of the memory block that needs to
+/// be read from the process identified by the `pid`.
+/// * `buff`: The `buff` parameter in the `read_process_address` function is a mutable pointer to an array of 128
+/// unsigned 8-bit integers (`[u8; 128]`). This pointer is used to store the bytes copied from a
+/// specific memory address within a process identified by the `pid` parameter.
+///
+/// Returns:
+///
+/// The function `read_process_address` is returning a `std::io::Result<()>`.
+fn read_process_address(
+    pid: Pid,
+    address: usize,
+    size: usize,
+    buff: *mut [u8; 128],
+) -> std::io::Result<()> {
     let handle: ProcessHandle = pid.try_into()?;
     let bytes = copy_address(address, size, &handle)?;
     for i in &bytes {
