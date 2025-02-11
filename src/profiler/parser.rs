8dacd3c2dafd370c09e725cdd80f7379b89a0bee
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-02-11 18:41:45 +0100

    feat: enhance parse_bin function to read binary data and identify Mach-O format

--- src/profiler/parser.rs
+++ src/profiler/parser.rs
@@ -1,4 +1,10 @@
-use std::path::Path;
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+    path::Path,
+};
+
+// use bincode::{self};
 
 use crate::{logs, utils};
 
@@ -8,4 +14,20 @@ pub fn parse_bin(pid: i32) {
         logs::error_log("Cannot find the binary of the process".to_string());
     }
     logs::info_log("Binary found".to_string());
+    let my_buf = BufReader::new(File::open(output).unwrap());
+    let mut bytes_vec: Vec<u8> = Vec::new();
+    for byte_or_error in my_buf.bytes() {
+        let byte = byte_or_error.unwrap();
+        bytes_vec.push(byte);
+    }
+    // read the magic number of the binary to find the format
+    // match the binary format in little endian
+    if bytes_vec.len() >= 4 && &bytes_vec[0..4] == [0xCF, 0xFA, 0xED, 0xFE] {
+        logs::info_log("Binary format is Mach-O".to_string());
+    }
+    // let decoded: Result<String, _> = bincode::deserialize(&bytes_vec);
+    // match decoded {
+    //     Ok(data) => println!("{:?}", data),
+    //     Err(e) => println!("Deserialization error: {:?}", e),
+    // }
 }
